19953d05bafb10f50331c03420abe690ac124832
Zane Rockenbaugh <zane@liquid-labs.com>
2018-10-22 22:41:02 -0500

    our first toolkit function; 'find-exec' and 'require-exec'

---
+++ src/files/find-exec.func.sh
@@ -0,0 +1,30 @@
+function find-exec() {
+  local EXEC_NAME="$1"
+  local PACKAGE_ROOT="${2:-}"
+
+  local EXEC=''
+  # prefer the package-local exec, if available
+  if [[ -n "$PACKAGE_ROOT" ]]; then
+    pushd "$PACKAGE_ROOT" > /dev/null
+    # Search for the exec locall, then globally.
+    EXEC=$(npm bin)/$EXEC_NAME
+    popd > /dev/null
+  fi
+  if [[ ! -x "$EXEC" ]]; then
+    if which -s catalyst-scripts; then
+      EXEC='catalyst-scripts'
+    else
+      return 10
+    fi
+  fi
+
+  echo $EXEC
+}
+
+function require-exec() {
+  local EXEC_NAME="$1"
+  if ! find-exec "$@"; then
+    echo "Could not locate executable '$EXEC_NAME'; bailing out." >&2
+    exit 10
+  fi
+}
