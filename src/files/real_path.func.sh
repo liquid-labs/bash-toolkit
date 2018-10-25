3fc650c60a155a5bd051ba85bb6f1ab32cae9b78
Zane Rockenbaugh <zane@liquid-labs.com>
2018-10-24 19:55:17 -0500

    final testing abstraction

--- src/files/real_path.func.sh
+++ src/files/real_path.func.sh
@@ -9,29 +9,30 @@ function real_path {
   fi
 
   if [[ -h "$FILE" ]]; then
+    function resolve_link {
+      local POSSIBLE_REL_LINK="${1:-}"
+      local APPEND="${2:-}"
+      if [[ "$POSSIBLE_REL_LINK" == /* ]]; then
+        echo "$POSSIBLE_REL_LINK${APPEND}"
+      else
+        # now we go into the dir containg the link and then navigate the possibly
+        # relative link to the real dir
+        cd "$START_DIR"
+        cd "$(dirname "$FILE")"
+        cd "$POSSIBLE_REL_LINK"
+        echo "${PWD}${APPEND}"
+        cd "$START_DIR"
+      fi
+    }
+
     # local START_DIR="$PWD"
     if [[ ! -d "$FILE" ]]; then
       # we need to get the real path to the real file
-      local START_DIR="$PWD"
       local REAL_FILE_LINK_PATH="$(readlink "$FILE")"
-      local POSSIBLE_REL_LINK="$(dirname "$REAL_FILE_LINK_PATH")"
-      # now we go into the dir containg the link and then navigate the possibly
-      # relative link to the real dir
-      cd "$(dirname "$FILE")"
-      cd "$POSSIBLE_REL_LINK"
-      echo "$PWD/$(basename "$REAL_FILE_LINK_PATH")"
-      cd "$START_DIR"
+      resolve_link "$(dirname "$REAL_FILE_LINK_PATH")" "/$(basename "$REAL_FILE_LINK_PATH")"
     else
       # we need to get the real path of the linked directory
-      local POSSIBLE_REL_LINK="$(readlink "$FILE")"
-      if [[ "$POSSIBLE_REL_LINK" == /* ]]; then
-        echo "$POSSIBLE_REL_LINK"
-      else
-        cd "$(dirname "$FILE")"
-        cd "$POSSIBLE_REL_LINK"
-        echo "$PWD"
-        cd "$START_DIR"
-      fi
+      resolve_link "$(readlink "$FILE")"
     fi
   else
     echo "$FILE"
