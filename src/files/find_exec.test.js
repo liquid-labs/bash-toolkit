12d341cbaf45de3483d2913c874b9f83707dd58d
Zane Rockenbaugh <zane@liquid-labs.com>
2018-10-24 20:02:05 -0500

    renamed find-exec to find_exec

--- src/files/find_exec.test.js
+++ src/files/find_exec.test.js
@@ -6,8 +6,8 @@ const execOpts = {
   silent : true,
 }
 
-test('find-exec will find npm-local executables', () => {
-  const result = shell.exec(`source src/files/find-exec.func.sh && find-exec eslint "$PWD"`, execOpts)
+test('find_exec will find npm-local executables', () => {
+  const result = shell.exec(`source src/files/find_exec.func.sh && find_exec eslint "$PWD"`, execOpts)
   const expectedOut = expect.stringMatching(/eslint\s*$/)
 
   expect(result.stderr).toEqual('')
@@ -15,8 +15,8 @@ test('find-exec will find npm-local executables', () => {
   expect(result.code).toBe(0)
 })
 
-test('find-exec will search multiple NPM directories', () => {
-  const result = shell.exec(`source src/files/find-exec.func.sh && find-exec eslint /tmp /etc "$PWD"`, execOpts)
+test('find_exec will search multiple NPM directories', () => {
+  const result = shell.exec(`source src/files/find_exec.func.sh && find_exec eslint /tmp /etc "$PWD"`, execOpts)
   const expectedOut = expect.stringMatching(/eslint\s*$/)
 
   expect(result.stderr).toEqual('')
@@ -24,31 +24,31 @@ test('find-exec will search multiple NPM directories', () => {
   expect(result.code).toBe(0)
 })
 
-test('find-exec will find global executables', () => {
-  let result = shell.exec(`source src/files/find-exec.func.sh && find-exec bash "$PWD"`, execOpts)
+test('find_exec will find global executables', () => {
+  let result = shell.exec(`source src/files/find_exec.func.sh && find_exec bash "$PWD"`, execOpts)
   const expectedOut = expect.stringMatching(/bash\s*$/)
 
   expect(result.stderr).toEqual('')
   expect(result.stdout).toEqual(expectedOut)
   expect(result.code).toBe(0)
 
-  result = shell.exec(`source src/files/find-exec.func.sh && find-exec bash`, execOpts)
+  result = shell.exec(`source src/files/find_exec.func.sh && find_exec bash`, execOpts)
   expect(result.stderr).toEqual('')
   expect(result.stdout).toEqual(expectedOut)
   expect(result.code).toBe(0)
 })
 
-test('find-exec and require-exec will fail when no exec found', () =>{
+test('find_exec and require-exec will fail when no exec found', () =>{
   // supresses err echo from shelljs
   console.error = jest.fn() // eslint-disable-line no-console
-  let result = shell.exec(`source src/files/find-exec.func.sh && find-exec fizzyboo "$PWD"`, execOpts)
+  let result = shell.exec(`source src/files/find_exec.func.sh && find_exec fizzyboo "$PWD"`, execOpts)
   const expectedErr = expect.stringContaining("Could not locate executable 'fizzyboo'")
 
   expect(result.stderr).toEqual('')
   expect(result.stdout).toEqual('')
   expect(result.code).toBe(10)
 
-  result = shell.exec(`source src/files/find-exec.func.sh && require-exec fizzyboo`, execOpts)
+  result = shell.exec(`source src/files/find_exec.func.sh && require-exec fizzyboo`, execOpts)
   expect(result.stderr).toEqual(expectedErr)
   expect(result.stdout).toEqual('')
   expect(result.code).toBe(10)
