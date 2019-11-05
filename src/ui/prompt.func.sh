ef1b1354c93f3d14fa88a78421ffb94b55347062
Zane Rockenbaugh <zane@liquid-labs.com>
2019-11-05 16:36:55 -0600

    added 'get-answer' with multi-line support

--- src/ui/prompt.func.sh
+++ src/ui/prompt.func.sh
@@ -1,11 +1,48 @@
 import echoerr
+import lists
 import options
+
+get-answer() {
+  eval "$(setSimpleOptions MULTI_LINE -- "$@")"
+  local PROMPT="$1"
+  local VAR="$2" # TODO: if name is 'VAR', then this breaks...
+  local DEFAULT="${3:-}"
+
+  if [[ -n "$FORCE" ]] && [[ -z "$DEFAULT" ]]; then
+    DEFAULT="${!VAR}"
+  fi
+
+  if [[ -n "${DEFAULT}" ]]; then
+    PROMPT="${PROMPT} (${DEFAULT}) "
+  fi
+
+  if [[ -z "$MULTI_LINE" ]]; then
+    read -r -p "$PROMPT" $VAR
+    if [[ -z ${!VAR:-} ]] && [[ -n "$DEFAULT" ]]; then
+      # MacOS dosen't support 'declare -g' :(
+      eval "${VAR}='$(echo "$DEFAULT" | sed "s/'/'\"'\"'/g")'"
+    fi
+  else
+    local LINE
+    echo "${green_bu}End multi-line input with single '.'${reset}"
+    echo "$PROMPT"
+    while true; do
+      read -r LINE
+      [[ "$LINE" == '.' ]] && break
+      list-add-item $VAR "$LINE"
+    done
+  fi
+}
 
 require-answer() {
   eval "$(setSimpleOptions FORCE -- "$@")"
   local PROMPT="$1"
   local VAR="$2" # TODO: if name is 'VAR', then this breaks...
   local DEFAULT="${3:-}"
+
+  if [[ -n "$FORCE" ]] && [[ -z "$DEFAULT" ]]; then
+    DEFAULT="${!VAR}"
+  fi
 
   if [[ -n "${DEFAULT}" ]]; then
     PROMPT="${PROMPT} (${DEFAULT}) "
@@ -18,9 +55,9 @@ require-answer() {
     elif [[ -z ${!VAR:-} ]] && [[ -n "$DEFAULT" ]]; then
       # MacOS dosen't support 'declare -g' :(
       eval "${VAR}='$(echo "$DEFAULT" | sed "s/'/'\"'\"'/g")'"
-      unset FORCE
+      FORCE=''
     elif [[ -n "${!VAR:-}" ]] && [[ -n "$FORCE" ]]; then
-      unset FORCE
+      FORCE=''
     fi
   done
 }
