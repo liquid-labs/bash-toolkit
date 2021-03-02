5a307303a6a7ac94df82587dbd9f61cd7ed5e46e
Zane Rockenbaugh <zane@liquid-labs.com>
2021-03-02 09:41:06 -0600

    hotfix: distribute source and 'pkg's

--- Makefile
+++ Makefile
@@ -2,22 +2,25 @@
 
 default: all
 
-BUNDLES:=$(shell cd src && find . -name "*.*.sh") # BUNDLES is a list with items like './ui/colors.vars.sh'
-DIST_BUNDLES:=$(addprefix dist/, $(BUNDLES)) # DIST_BUNDLES like 'dist/./ui/colors.vars.sh'
-SRC_BUNDLES:=$(addprefix src/, $(BUNDLES)) # DIST_BUNDLES like 'dist/./ui/colors.vars.sh'
+BUNDLES:=$(shell cd src && find . -name "*.*.sh") # e.g.: ./ui/colors.vars.sh
+SRC_BUNDLES:=$(addprefix src/, $(BUNDLES)) # e.g.: dist/./ui/colors.vars.sh
+DIST_BUNDLES:=$(addprefix dist/, $(BUNDLES)) # e.g.: dist/./ui/colors.vars.sh
+PKG_BUNDLES:=$(addsuffix .pkg.sh, $(basename $(DIST_BUNDLES))) # e.g.: dist/./ui/colors.vars.pkg.sh
 
 BASH_ROLLUP:=$(shell npm bin)/bash-rollup
 
-all: $(DIST_BUNDLES)
+all: $(DIST_BUNDLES) $(PKG_BUNDLES)
 
 clean:
 	rm -rf dist
 
 # We could explicitly specify all the depenncies, but let's keep it simple. This will perform unecessary builds.
-$(DIST_BUNDLES): dist/%: src/% $(SRC_BUNDLES)
+$(PKG_BUNDLES): dist/%.pkg.sh: src/%.sh $(SRC_BUNDLES)
 	mkdir -p $(dir $@)
 	$(BASH_ROLLUP) $< $@
 
-# include $(DIST_BUNDLES)
+$(DIST_BUNDLES): dist/%: src/%
+	mkdir -p $(dir $@)
+	cp $< $@
 
 .PHONY: all clean
--- dist/ui/select.func.pkg.sh
+++ dist/ui/select.func.pkg.sh
@@ -14,7 +14,183 @@
 # combined and the presece of a newline in any option will cause the input to
 # interepretted by the second method.
 
-import lists
+list-add-item() {
+  local LIST_VAR="${1}"; shift
+  while (( $# > 0 )); do
+    local ITEM
+    ITEM="${1}"; shift
+    # TODO: enforce no newlines in item
+
+    if [[ -n "$ITEM" ]]; then
+      if [[ -z "${!LIST_VAR:-}" ]]; then
+        eval $LIST_VAR='"$ITEM"'
+      else
+        # echo $LIST_VAR='"${!LIST_VAR}"$'"'"'\n'"'"'"${ITEM}"'
+        eval $LIST_VAR='"${!LIST_VAR}"$'"'"'\n'"'"'"${ITEM}"'
+      fi
+    fi
+  done
+}
+
+list-add-uniq() {
+  local LIST_VAR="${1}"; shift
+  while (( $# > 0 )); do
+    local ITEM
+    ITEM="${1}"; shift
+    # TODO: enforce no newlines in item
+    if [[ -z $(list-get-index $LIST_VAR "$ITEM") ]]; then
+      list-add-item $LIST_VAR "$ITEM"
+    fi
+  done
+}
+
+# Echos the number of items in the list.
+#
+# Takes single argument, the list var name.
+#
+# Example:
+# list-add-item A B C
+# list-count MY_LIST # echos '3'
+list-count() {
+  if [[ -z "${!1:-}" ]]; then
+    echo -n "0"
+  else
+    echo -e "${!1}" | wc -l | tr -d '[:space:]'
+  fi
+}
+
+list-from-csv() {
+  local LIST_VAR="${1}"
+  local CSV="${2:-}"
+
+  if [[ -z "$CSV" ]]; then
+    CSV="${!LIST_VAR}"
+    unset ${LIST_VAR}
+  fi
+
+  if [[ -n "$CSV" ]]; then
+    local ADDR
+    while IFS=',' read -ra ADDR; do
+      for i in "${ADDR[@]}"; do
+        i="$(echo "$i" | awk '{$1=$1};1')"
+        list-add-item "$LIST_VAR" "$i"
+      done
+    done <<< "$CSV"
+  fi
+}
+
+list-get-index() {
+  local LIST_VAR="${1}"
+  local TEST="${2}"
+
+  local ITEM
+  local INDEX=0
+  while read -r ITEM; do
+    if [[ "${ITEM}" == "${TEST}" ]]; then
+      echo $INDEX
+      return
+    fi
+    INDEX=$(($INDEX + 1))
+  done <<< "${!LIST_VAR:-}"
+}
+
+list-get-item() {
+  local LIST_VAR="${1}"
+  local INDEX="${2}"
+
+  local CURR_INDEX=0
+  local ITEM
+  while read -r ITEM; do
+    if (( $CURR_INDEX == $INDEX )) ; then
+      echo -n "${ITEM%\\n}"
+      return
+    fi
+    CURR_INDEX=$(($CURR_INDEX + 1))
+  done <<< "${!LIST_VAR}"
+}
+
+# Echoes the frist item in the named list matching the given prefix.
+#
+# Example:
+# LIST="foo bar"$'\n'"foo baz"
+# list-get-item-by-prefix LIST "foo " # echoes 'foo bar'
+list-get-item-by-prefix() {
+  local LIST_VAR="${1}"
+  local PREFIX="${2}"
+
+  local ITEM
+  while read -r ITEM; do
+    if [[ "${ITEM}" == "${PREFIX}"* ]] ; then
+      echo -n "${ITEM%\\n}"
+      return
+    fi
+  done <<< "${!LIST_VAR}"
+}
+
+# Joins a list with a given string and echos the result. We use 'echo -e' for the join string, so '\n', '\t', etc. will
+# work.
+#
+# Takes (1) the list variable name, (2) the join string
+#
+# Example:
+# list-add-item MY_LIST A B C
+# list-join MY_LIST '||' # echos 'A||B||C'
+list-join() {
+  local LIST_VAR="${1}"
+  local JOIN_STRING="${2}"
+
+  local CURR_INDEX=0
+  local COUNT
+  COUNT="$(list-count $LIST_VAR)"
+  while read -r ITEM; do
+    echo -n "$ITEM"
+    CURR_INDEX=$(($CURR_INDEX + 1))
+    if (( $CURR_INDEX < $COUNT )) ; then
+      echo -ne "$JOIN_STRING"
+    fi
+  done <<< "${!LIST_VAR}"
+}
+
+list-replace-by-string() {
+  local LIST_VAR="${1}"
+  local TEST_ITEM="${2}"
+  local NEW_ITEM="${3}"
+
+  local ITEM INDEX NEW_LIST
+  INDEX=0
+  for ITEM in ${!LIST_VAR:-}; do
+    if [[ "$(list-get-item $LIST_VAR $INDEX)" == "$TEST_ITEM" ]]; then
+      list-add-item NEW_LIST "$NEW_ITEM"
+    else
+      list-add-item NEW_LIST "$ITEM"
+    fi
+    INDEX=$(($INDEX + 1))
+  done
+  eval $LIST_VAR='"'"$NEW_LIST"'"'
+}
+
+list-quote() {
+  local LIST_VAR="${1}"
+
+  while read -r ITEM; do
+    echo -n "'$(echo "$ITEM" | sed -e "s/'/'\"'\"'/")' "
+  done <<< "${!LIST_VAR:-}"
+}
+
+list-rm-item() {
+  local LIST_VAR="${1}"; shift
+  while (( $# > 0 )); do
+    local ITEM NEW_ITEMS
+    ITEM="${1}"; shift
+    ITEM=${ITEM//\/\\/}
+    ITEM=${ITEM//#/\\#}
+    ITEM=${ITEM//./\\.}
+    ITEM=${ITEM//[/\\[}
+    # echo "ITEM: $ITEM" >&2
+    NEW_ITEMS="$(echo "${!LIST_VAR}" | sed -e '\#^'"${ITEM}"'$#d')"
+    eval $LIST_VAR='"'"$NEW_ITEMS"'"'
+  done
+}
 
 _commonSelectHelper() {
   # TODO: the '_' is to avoid collision, but is a bit hacky; in particular, some callers were using 'local OPTIONS'
