c4d0f282d495dd4d980bb4872867c4665fe16c72
Zane Rockenbaugh <zane@liquid-labs.com>
2021-03-02 09:24:14 -0600

    hotfix: suitable for '-o nounset'

--- dist/bash-toolkit.funcs.sh
+++ dist/bash-toolkit.funcs.sh
@@ -685,20 +685,20 @@ echofmt() {
 
   # First, let's check to see of the message is suppressed. The 'return 0' is explicitly necessary. 'return' sends
   # along $?, which, if it gets there, is 1 due to the failed previous test.
-  ! { [[ -n "${ECHO_SILENT}" ]] && [[ -z "${ERROR}" ]]; } || return 0
-  ! { [[ -n "${ECHO_QUIET}" ]] && [[ -z "${ERROR}" ]] && [[ -z "${WARN}" ]]; } || return 0
+  ! { [[ -n "${ECHO_SILENT:-}" ]] && [[ -z "${ERROR:-}" ]]; } || return 0
+  ! { [[ -n "${ECHO_QUIET:-}" ]] && [[ -z "${ERROR:-}" ]] && [[ -z "${WARN}" ]]; } || return 0
 
   # Examine environment to see if the redirect controls are set.
-  if [[ -z "${STDERR}" ]] && [[ -z "${STDOUT}" ]]; then
-    [[ -z "${ECHO_STDERR}" ]] || STDERR=true
-    [[ -z "${ECHO_STDOUT}" ]] || STDOUT=true
+  if [[ -z "${STDERR:-}" ]] && [[ -z "${STDOUT:-}" ]]; then
+    [[ -z "${ECHO_STDERR:-}" ]] || STDERR=true
+    [[ -z "${ECHO_STDOUT:-}" ]] || STDOUT=true
   fi
 
   # Determine width... if folding
-  [[ -z "${NO_FOLD}" ]] && [[ -n "${WIDTH}" ]] || { # If width is set as an option, then that's the end of story.
+  [[ -z "${NO_FOLD:-}" ]] && [[ -n "${WIDTH:-}" ]] || { # If width is set as an option, then that's the end of story.
     local DEFAULT_WIDTH=82
-    local WIDTH="${ECHO_WIDTH}"
-    [[ -n "${WIDTH}" ]] || WIDTH=$DEFAULT_WIDTH
+    local WIDTH="${ECHO_WIDTH:-}"
+    [[ -n "${WIDTH:-}" ]] || WIDTH=$DEFAULT_WIDTH
     # ECHO_WIDTH and DEFAULT_WIDTH are both subject to actual terminal width limitations.
     local TERM_WIDITH
     TERM_WIDTH=$(tput cols)
@@ -708,31 +708,31 @@ echofmt() {
   # Determine output color, if any.
   # internal helper function; set's 'STDERR' true unless target has already been set with '--stderr' or '--stdout'
   default-stderr() {
-    [[ -n "${STDERR}" ]] || [[ -n "${STDOUT}" ]] || STDERR=true
+    [[ -n "${STDERR:-}" ]] || [[ -n "${STDOUT:-}" ]] || STDERR=true
   }
   local COLOR=''
-  if [[ -n "${ERROR}" ]]; then
+  if [[ -n "${ERROR:-}" ]]; then
     COLOR="${red}"
     default-stderr
-  elif [[ -n "${WARN}" ]]; then
+  elif [[ -n "${WARN:-}" ]]; then
     COLOR="${yellow}"
     default-stderr
-  elif [[ -n "${INFO}" ]]; then
+  elif [[ -n "${INFO:-}" ]]; then
     COLOR="${green}"
   fi
 
   # we don't want to use an eval, and the way bash is evaluated means we can't do 'echo ... ${REDIRECT}' or something.
-  if [[ -n "${STDERR}" ]]; then
+  if [[ -n "${STDERR:-}" ]]; then
     if [[ -z "$NO_FOLD" ]]; then
-      echo -e "${COLOR}$*${reset}" | fold -sw "${WIDTH}" >&2
+      echo -e "${COLOR:-}$*${reset}" | fold -sw "${WIDTH}" >&2
     else
-      echo -e "${COLOR}$*${reset}" >&2
+      echo -e "${COLOR:-}$*${reset}" >&2
     fi
   else
-    if [[ -z "$NO_FOLD" ]]; then
-      echo -e "${COLOR}$*${reset}" | fold -sw "${WIDTH}"
+    if [[ -z "${NO_FOLD:-}" ]]; then
+      echo -e "${COLOR:-}$*${reset}" | fold -sw "${WIDTH}"
     else
-      echo -e "${COLOR}$*${reset}"
+      echo -e "${COLOR:-}$*${reset}"
     fi
   fi
 }
--- js/ui/echofmt.test.js
+++ js/ui/echofmt.test.js
@@ -2,6 +2,7 @@
 import { assertMatchNoError, shell, execOpts } from '../testlib'
 
 const COMPILE_EXEC = 'source dist/ui/echofmt.func.sh'
+const STRICT = 'set -o errexit; set -o nounset; set -o pipefail'
 
 // TODO: We would like to make the output test more precise, but can't seem to find something that matches the color
 // control characters. We tried both '[^ -~]' and '[^\x20-\x7E]' to cover 'non-printable' characters, but neither
@@ -15,13 +16,13 @@ const checkQuiteEchos = (result) => {
 
 describe('echofmt', () => {
   test('echo hi', () => {
-    const result = shell.exec(`set -e; ${COMPILE_EXEC}; echofmt hi`, execOpts)
+    const result = shell.exec(`${STRICT}; ${COMPILE_EXEC}; echofmt hi`, execOpts)
     const expectedOut = expect.stringMatching(/^.*hi.*\n$/)
     assertMatchNoError(result, expectedOut)
   })
 
   test('echo "hi\nbye"', () => {
-    const result = shell.exec(`set -e; ${COMPILE_EXEC}; echofmt "hi\nbye"`, execOpts)
+    const result = shell.exec(`${STRICT}; ${COMPILE_EXEC}; echofmt "hi\nbye"`, execOpts)
     const expectedOut = expect.stringMatching(/^.*hi\nbye.*\n$/)
     assertMatchNoError(result, expectedOut)
   })
@@ -29,51 +30,51 @@ describe('echofmt', () => {
   test('folds longer lines', () => {
     // 80 x a + ' abc' makes this 84 long and should trigger default fold
     let ax80 = 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
-    const result = shell.exec(`set -e; ${COMPILE_EXEC}; echofmt ${ax80} abc`, execOpts)
+    const result = shell.exec(`${STRICT}; ${COMPILE_EXEC}; echofmt ${ax80} abc`, execOpts)
     const expectedOut = expect.stringMatching(new RegExp(`^.*${ax80} [\n]abc.*\n$`))
     assertMatchNoError(result, expectedOut)
   })
 
   describe('when ECHO_QUIET=true', () => {
     test('supresses regular messages', () => {
-      const result = shell.exec(`set -e; ${COMPILE_EXEC}; ECHO_QUIET=true echofmt hi`, execOpts)
+      const result = shell.exec(`${STRICT}; ${COMPILE_EXEC}; ECHO_QUIET=true echofmt hi`, execOpts)
       assertMatchNoError(result, '')
     })
 
     test('supresses info messages', () => {
-      const result = shell.exec(`set -e; ${COMPILE_EXEC}; ECHO_QUIET=true echofmt --info hi`, execOpts)
+      const result = shell.exec(`${STRICT}; ${COMPILE_EXEC}; ECHO_QUIET=true echofmt --info hi`, execOpts)
       assertMatchNoError(result, '')
     })
 
     test('echos warning messages', () => {
-      const result = shell.exec(`set -e; ${COMPILE_EXEC}; ECHO_QUIET=true echofmt --warn hi`, execOpts)
+      const result = shell.exec(`${STRICT}; ${COMPILE_EXEC}; ECHO_QUIET=true echofmt --warn hi`, execOpts)
       checkQuiteEchos(result)
     })
 
     test('echoes error messages', () => {
-      const result = shell.exec(`set -e; ${COMPILE_EXEC}; ECHO_QUIET=true echofmt --error hi`, execOpts)
+      const result = shell.exec(`${STRICT}; ${COMPILE_EXEC}; ECHO_QUIET=true echofmt --error hi`, execOpts)
       checkQuiteEchos(result)
     })
   })
 
   describe('when ECHO_SILENT=true', () => {
     test('supresses regular messages', () => {
-      const result = shell.exec(`set -e; ${COMPILE_EXEC}; ECHO_SILENT=true echofmt hi`, execOpts)
+      const result = shell.exec(`${STRICT}; ${COMPILE_EXEC}; ECHO_SILENT=true echofmt hi`, execOpts)
       assertMatchNoError(result, '')
     })
 
     test('supresses info messages', () => {
-      const result = shell.exec(`set -e; ${COMPILE_EXEC}; ECHO_SILENT=true echofmt --info hi`, execOpts)
+      const result = shell.exec(`${STRICT}; ${COMPILE_EXEC}; ECHO_SILENT=true echofmt --info hi`, execOpts)
       assertMatchNoError(result, '')
     })
 
     test('supresses warning messages', () => {
-      const result = shell.exec(`set -e; ${COMPILE_EXEC}; ECHO_SILENT=true echofmt --warn hi`, execOpts)
+      const result = shell.exec(`${STRICT}; ${COMPILE_EXEC}; ECHO_SILENT=true echofmt --warn hi`, execOpts)
       assertMatchNoError(result, '')
     })
 
     test('echoes error messages', () => {
-      const result = shell.exec(`set -e; ${COMPILE_EXEC}; ECHO_SILENT=true echofmt --error hi`, execOpts)
+      const result = shell.exec(`${STRICT}; ${COMPILE_EXEC}; ECHO_SILENT=true echofmt --error hi`, execOpts)
       checkQuiteEchos(result)
     })
   })
