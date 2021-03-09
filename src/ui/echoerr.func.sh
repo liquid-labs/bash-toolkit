6d953f52010d6892a180b8880d39350d674dfe50
Zane Rockenbaugh <zane@liquid-labs.com>
2021-03-08 23:00:20 -0600

    hotfix: support exit override

--- src/ui/echoerr.func.sh
+++ src/ui/echoerr.func.sh
@@ -11,12 +11,18 @@ echowarn() {
 # Echoes a formatted message to STDERR. The default exit code is '1', but if 'EXIT_CODE', then that will be used. E.g.:
 #
 #    EXIT_CODE=5
-#    echoerrandexit "Fatal code 5!"
+#    echoerrandexit "Fatal code 5
+#
+# 'ECHO_NEVER_EXIT' can be set to any non-falsy value to supress the exit. This is intended primarily for use when liq
+# functions are sourced and called directly from within the main shell, in which case exiting would kill the entire
+# terminal session.
 #
 # See echofmt for further options and details.
 echoerrandexit() {
   echofmt --error "$@"
 
-  [[ -z "${EXIT_CODE:-}" ]] || exit ${EXIT_CODE}
-  exit 1
+  if [[ -z "${ECHO_NEVER_EXIT}" ]]; then
+    [[ -z "${EXIT_CODE:-}" ]] || exit ${EXIT_CODE}
+    exit 1
+  fi
 }
