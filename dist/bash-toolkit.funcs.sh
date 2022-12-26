9863849f9a96c23edab1ba95303d7420cbb2036c
Zane Rockenbaugh <zane@liquid-labs.com>
2022-12-26 09:54:04 -0600

    remove dist

--- dist/bash-toolkit.funcs.sh
+++
@@ -1,4 +0,0 @@
-source ./cli/index.sh
-source ./data/index.sh
-source ./files/index.sh
-source ./ui/index.sh
--- dist/bash/strict.inline.sh
+++
@@ -1,4 +0,0 @@
-# bash strict settings
-set -o errexit # exit on errors; set -e
-set -o nounset # exit on use of uninitialized variable
-set -o pipefail # exit if any part of a pipeline fails (rather than just on failure of final piece)
